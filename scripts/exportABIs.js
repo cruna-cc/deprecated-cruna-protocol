286880023cd6932387b86d453686cfe5de0ea479
Francesco Sullo <sullof@gmail.com>
2023-06-03 16:07:41 -0700

    Improve export to include libs

--- scripts/exportABIs.js
+++ scripts/exportABIs.js
@@ -8,17 +8,21 @@ async function main() {
   };
 
   function abi(name, folder, rename) {
-    let source = path.resolve(__dirname, `../artifacts/contracts/${folder ? folder + "/" : ""}${name}.sol/${name}.json`);
+    let source = path.resolve(__dirname, `../artifacts/${folder ? folder + "/" : ""}${name}.sol/${name}.json`);
     let json = require(source);
     ABIs.contracts[rename || name] = json.abi;
   }
-  abi("CrunaVault", "implementation");
-  abi("ERC6551Registry", "bound-account");
-  abi("ERC6551Account", "bound-account");
-  abi("ERC6551AccountUpgradeable", "bound-account");
-  abi("ERC6551AccountProxy", "bound-account");
-  abi("TransparentVault", "vaults");
-  abi("TokenUtils", "utils");
+  abi("CrunaVault", "contracts/implementation");
+  abi("ERC6551Registry", "contracts/bound-account");
+  abi("ERC6551Account", "contracts/bound-account");
+  abi("ERC6551AccountUpgradeable", "contracts/bound-account");
+  abi("ERC6551AccountProxy", "contracts/bound-account");
+  abi("TransparentVault", "contracts/vaults");
+  abi("TokenUtils", "contracts/utils");
+
+  abi("ERC20", "@openzeppelin/contracts/token/ERC20");
+  abi("ERC721", "@openzeppelin/contracts/token/ERC721");
+  abi("ERC1155", "@openzeppelin/contracts/token/ERC1155");
 
   await fs.writeFile(path.resolve(__dirname, "../export/ABIs.json"), JSON.stringify(ABIs, null, 2));
 }
