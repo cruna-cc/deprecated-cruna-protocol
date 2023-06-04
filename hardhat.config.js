39a65b2198ff5d0e46c8091d5e90fca363cbc12f
Francesco Sullo <sullof@gmail.com>
2023-06-03 19:10:08 -0700

    Use conditional loading for cryptoenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,34 +1,22 @@
 const {requirePath} = require("require-or-mock");
-// if missed, it sets up a mock
+// if missing, it sets up a mock
 requirePath(".env");
 requirePath("export/deployed.json");
 
 require("dotenv").config();
-require("@secrez/cryptoenv").parse(() => !process.env.SKIP_CRYPTOENV && process.env.NODE_ENV !== "test");
+if (!process.env.SKIP_CRYPTOENV && process.env.NODE_ENV !== "test") {
+  require("@secrez/cryptoenv").parse();
+}
 
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("@openzeppelin/hardhat-upgrades");
 require("hardhat-contract-sizer");
 require("solidity-coverage");
-const requireOrMock = require("require-or-mock");
 
 if (process.env.GAS_REPORT === "yes") {
   require("hardhat-gas-reporter");
 }
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
